8aa49668262ed9b40788153d9c26fe4ec34d20ca
Marco Antonio Gil <gildo_marco@hotmail.com>
2024-11-27 16:54:59 -0300

    configurações de deploy

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/desktop/', // Adicione esta linha
   plugins: [react()],
+  base: '/desktop/', // Adicione esta linha
   test: {
     globals: true,
     environment: 'jsdom',

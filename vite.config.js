1de1aeb3ea9701763552bc6da430067dc7749c9a
Marco Antonio Gil <gildo_marco@hotmail.com>
2024-11-27 16:55:12 -0300

    configurações

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/desktop/', // Adicione esta linha
+  base: '/desktop', // Adicione esta linha
   test: {
     globals: true,
     environment: 'jsdom',

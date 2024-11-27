d3b752af749e0e992c604e58b9b324eab277e8d7
Marco Antonio Gil <gildo_marco@hotmail.com>
2024-11-27 15:47:51 -0300

    teste

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Rotas from './routes.jsx'
-import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/desktop/', // Adicione esta linha
   plugins: [react()],
   test: {
     globals: true,
